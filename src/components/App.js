675275b391510e63d89a47e3f2e4f99f1625c6ef
Yashjain0007 <yashjain3446@gmail.com>
2023-06-25 23:56:48 +0530

    submitting code-Sun Jun 25 2023 23:56:36 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  let [count, setCount] = useState(0); //cur val
+  let [sum, setSum] = useState(0); //cur sum
+  console.log(count);
+
+  // function check(count){
+
+  //   if(count>0){
+  //     setSum((Number(sum))+(Number(count)))
+
+  //   }
+  //   else if(count<0){
+  //     setSum((Number(sum))-(Number(count)))
+  //     console.log(count);
+  //   }
+  //   // console.log(typeof Number(sum))
+  // }
+  // console.log(sum)
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h2>Sum Calculator</h2>
+      <input
+        type="number"
+        onChange={(e) => {
+          setSum(sum + Number(e.target.value));
+          setCount(e.target.value);
+          // check(count)
+        }}
+        value={count}
+      />
+      <p>Sum: {sum}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
